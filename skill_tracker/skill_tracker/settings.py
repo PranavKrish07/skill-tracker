da0301709bb765d31adfd5d84918cf00b5d7b19b
Pranav Krishna <pranavkrishna404@gmail.com>
2025-09-28 14:34:52 +0530

    followup, template connected

--- skill_tracker/skill_tracker/settings.py
+++ skill_tracker/skill_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#gd@lh7a2iu5qd28h88etf=ouq5v81c)$h#!ai!17lil=utkqv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'skill_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
