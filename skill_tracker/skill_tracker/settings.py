52cd962eb8204d6cf755823e353da3bdb0e0ecf9
Pranav Krishna <pranavkrishna404@gmail.com>
2025-10-25 18:02:52 +0530

    deployment settings changed

--- skill_tracker/skill_tracker/settings.py
+++ skill_tracker/skill_tracker/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-#gd@lh7a2iu5qd28h88etf=ouq5v81c)$h#!ai!17lil=utkqv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1']
 
 
 LOGIN_REDIRECT_URL = '/home/'
