87779c8003dbda426d700ee6fbe68e948cb76fb2
Pranav Krishna <pranavkrishna404@gmail.com>
2025-10-26 21:25:26 +0530

    Reapply "Delete patch update"
    This reverts commit 39a9a033e49083c95e62b6e0a50be45fadf77546.

--- skill_tracker/home/urls.py
+++ skill_tracker/home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('addskill/', views.addSkill, name='addskill'),
     path('<str:sk>/', views.skills, name='skills'),
+    path('<str:sk>/delete/', views.deleteSkill, name='delete_skill'),
 ]
